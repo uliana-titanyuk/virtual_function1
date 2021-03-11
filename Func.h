e7b7f97e4697196744d637fb8ea113a475c39ff1
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2021-03-11 09:57:24 +0300

    Create Func.h

---
+++ Func.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm> 
+#include <stdio.h>
+#include <math.h>
+#include "CRat.h"
+#include "CVectRat.h"
+using namespace std;
+
+
+CVectRat input(int size);
+void raschet(void);
+void autotest(void);
