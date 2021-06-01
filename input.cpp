9876cdea61de86935eef013f57f8fc5d3b7917a5
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2021-06-01 22:19:51 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -3,18 +3,29 @@
 #include "CVectRat.h"
 
 CVectRat input(int size) {
-	CVectRat m(size);
-	int num;
+	
+	int vecType = -1;
+	std::cout << "Input 0 for horizontal vector and 1 for vertical:\n";
+	std::cin >> vecType;
+	if (vecType !=0 && vecType != 1) {
+		std::cerr << "Error! Wrong input!\n";
+		exit(1);
+	}
+
+	CVectRat* m = vecType == 0 ? new gorCVectRat(size) : new vertCVectRat(size) ;
 	unsigned den;
 	for (int i = 0; i < size; i++) {
 		cout << "Vvedite " << i + 1 << "-yu koordinatu (chisl i znamen)\t";
 		cin >> num;
 		cin >> den;
 		CRat a(num, den);
-		m.Set_arr(i, a);
+		m->Set_arr(i, a);
 	}
 	cout << "Vy vveli vector\t\n\n";
-	m.print();
+	m->print();
 	cout << endl;
-	return m;
+
+	//delete m;
+
+	return *m;
 };
