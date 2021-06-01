7fba17f79bcd099a5dd3af69d519e862d437921e
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2021-06-01 22:20:26 +0300

    Update raschet.cpp

--- raschet.cpp
+++ raschet.cpp
@@ -7,30 +7,30 @@ void raschet(void) {
 	cout << "Vvedite dlinu vectora" << endl;
 	cin >> size;
 
-	CVectRat vect1(size);
-	CVectRat vect2(size);
+	/*CVectRat vect1(size);
+	CVectRat vect2(size);*/
 	gorCVectRat vect3(size);
 	vertCVectRat vect4(size);
 	CRat m;
 
-	vect1 = input(size);
-	vect2 = input(size);
+	/*vect1 = input(size);
+	vect2 = input(size);*/
 
 	cout << "________________________________________" << endl;
 	cout << "\n";
-	vect3 = vect1 + vect2;
-	vect3.gorprint();
-	vect4 = vect3
+	//vect3 = vect1 + vect2;
+	vect3.print();
+	vect4 = vect3;
+	std::cout << "\n";
+	//vect3 = vect1 - vect2;
+	vect3.print();
 	cout << "\n";
-	vect3 = vect1 - vect2;
-	vect3.gorprint();
-	cout << "\n";
-	m = vect1 * vect2;
+	//m = vect1 * vect2;
 	m.print();
 	cout << "\n";
-	vect4.vertprint();
+	vect4.print();
 	vect4 = vect3;
-	vect4.vertprint();
+	vect4.print();
 	m.print();
 	cout << "________________________________________" << endl;
 	cout << "\n";
